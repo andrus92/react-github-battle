75624e33de3fcf74ae491577eb9cdf614ec86857
andrus92 <yahoraual92@gmail.com>
2022-11-05 10:40:10 +0100

    Implemented detection of winner and looser

--- src/components/Battle/Results.js
+++ src/components/Battle/Results.js
@@ -1,4 +1,17 @@
-const Results = () => {
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { makeBattle } from "../api";
+
+const Results = (props) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(location.search);
+        const playerOne = searchParams.get('playerOneName');
+        const playerTwo = searchParams.get('playerTwoName');
+        //makeBattle()
+        const a = makeBattle(playerOne, playerTwo);
+    }, [])
     return (
         <h1>Results</h1>
     );
--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,66 @@
+const id = 'YOUR_CLIENT_ID';
+const sec  = 'YOUR_SECRET_ID';
+
+const params = `?client_id=${id}?client_secret=${sec}`;
+
+
+const getProfile = (userName) => fetch(`https://api.github.com/users/${userName}${params}`)
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } 
+        throw new Error(response.status);
+    })
+    .catch(error => handleError(error));
+
+const getRepos = (userName) => fetch(`https://api.github.com/users/${userName}/repos${params}&per_page=100`)
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } 
+        throw new Error(response.status);
+    })
+    .catch(error => handleError(error));
+
+
+const getStarCount = (repos) => {
+    return repos.reduce((acc, repo) => {
+        return acc = repo.stargazers_count;
+    }, 0)
+}
+
+const calculateScore = (profile, repos) => {
+    const followers = profile.followers;
+    const totalStars = getStarCount(repos);
+    return followers * 3 + totalStars;
+}
+
+const handleError = (error) => {
+    console.error(error);
+}
+
+const getUserData = async (userName) => {
+    let profile, repos;
+    profile = await getProfile(userName);
+    repos = await getRepos(userName);
+
+    return {
+        profile,
+        score: calculateScore(profile, repos),
+    }
+}
+
+export const makeBattle = async (userName1, userName2) => {
+    const a = await getUserData(userName1);
+    console.log(a)
+    //const b = await getUserData(userName2);
+}
+
+// const sortPlayers = ()
+
 export const fetchPopularRepos = (language, cbFunction) => {
     fetch(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
         .then(response => response.json())
         .then(data => cbFunction(data.items, language))
-        .catch(error => console.log(error));
+        .catch(error => handleError(error));
 }
-
