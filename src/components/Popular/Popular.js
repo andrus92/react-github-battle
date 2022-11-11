ad382237da8a7ade49fdd8822747b9682262a72a
andrus92 <yahoraual92@gmail.com>
2022-11-11 16:08:22 +0100

    Popular has been rewritten in class-mode just for practice

--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -2,18 +2,24 @@ import React from "react";
 import Repos from "./Repos";
 import Tabs from "./Tabs";
 import Loader from "../Loader";
-import { useSelector } from "react-redux";
+import { connect } from "react-redux";
 
-const Popular = (props) => {
-    const loading = useSelector(state => state.popularReducer.loading);
+const mapStateToProps = (state) => {
+    return {
+        loading: state.popularReducer.loading,
+    };
+} 
 
-    return (
-        <React.Fragment>
-            <Tabs />
-            <Loader loading={loading}/> 
-            <Repos />
-        </React.Fragment>
-    );
+class Popular extends React.Component {
+    render () {
+        return (
+            <React.Fragment>
+                <Tabs />
+                <Loader loading={this.props.loading}/> 
+                <Repos />
+            </React.Fragment>
+        );
+    }
 }
 
-export default Popular;
+export default connect(mapStateToProps)(Popular);
